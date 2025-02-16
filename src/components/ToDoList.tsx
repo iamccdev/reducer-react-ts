5b03eb76b6b9ab1d3f32f0b1926120a2c9956c08
iamccdev <iamccdev@gmail.com>
2025-02-16 00:54:31 -0500

    [Feat] actions and interface of todo list added

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,3 +1,5 @@
+import { useReducer, useState } from "react";
+
 type Todo = {
     id: number;
     text: string;
@@ -6,14 +8,77 @@ type State = {
     todos: Todo[];
 }
 
-type Action = 
-| { type: "ADD_TODO"; paylod: string }
-| { type: "REMOVE_TODO"; paylod: number }
+type Action =
+    | { type: "ADD_TODO"; payload: string }
+    | { type: "REMOVE_TODO"; payload: number }
 
-const initialState: State = { 
+const initialState: State = {
     todos: []
 }
 
-const todoReducer = (action: Action) => {
-    switch(Action.type)
+const todoReducer = (state: State, action: Action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            {
+                const newTodo: Todo = { id: state.todos.length + 1, text: action.payload }
+                return { todos: [...state.todos, newTodo] }
+            }
+        case "REMOVE_TODO": return { todos: state.todos.filter(todo => todo.id !== action.payload) }
+        default:
+            return state
+
+
+    }
 }
+
+const emojiMap: { [key: string]: string } = {
+    eat: "🍔",
+    sleep: "🛏️",
+    exercise: "🏋🏽",
+};
+
+const TodoList: React.FC = () => {
+    const [state, dispatch] = useReducer(todoReducer, initialState);
+    const [todoText, setTodoText] = useState("")
+
+    const handleAppTodo = () => {
+        const mappedText = emojiMap[todoText.toLowerCase() || todoText]
+        if (mappedText.trim()) {
+            dispatch({ type: "ADD_TODO", payload: mappedText })
+            setTodoText("")
+        }
+
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === "Enter") {
+            handleAppTodo();
+        }
+    }
+    return (
+        <div>
+            <em> Made with useReducer</em>
+            <h1>Emoji todo list</h1>
+            <input type="text"
+                value={todoText}
+                onChange={(e) => setTodoText(e.target.value)}
+                onKeyDown={handleKeyDown}
+                placeholder="Add a new todo"
+            />
+            <ul>
+                {
+                    state.todos.map((todo) => (
+                        <li
+                         key={todo.id}
+                         onClick={() => dispatch( { type: "REMOVE_TODO", payload: todo.id })}
+                        >
+                        {todo.text}
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import TodoList from './components/ToDoList.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TodoList />
   </StrictMode>,
 )
